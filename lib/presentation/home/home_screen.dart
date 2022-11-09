ce430731ef40aaa3acc7a5e4c6dff80642a8696b
JiyadAhammad <jiyadahammad99@gmail.com>
2022-11-09 16:11:10 +0530

    today weather report card finish

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../constant/color/colors.dart';
 import '../constant/sizedbox/sizedbox.dart';
@@ -28,39 +29,114 @@ class HomeScreen extends StatelessWidget {
               kheight,
               const MainHeadWidget(head: 'Weather Deatils'),
               kheight5,
-              SizedBox(
-                height: width * .4,
-                width: double.infinity,
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 6,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: backgrounColor(),
-                        child: const Card(
-                          color: ktransparent,
-                          child: WeatherDetailsWidget(
-                            content: 'khdfjl',
-                            value: 'dfsksjl',
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+              WeatheDetailcardWidget(width: width),
               kheight,
               const MainHeadWidget(head: 'Today'),
               kheight5,
+              TodayWeatherWidget(width: width),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TodayWeatherWidget extends StatelessWidget {
+  const TodayWeatherWidget({
+    super.key,
+    required this.width,
+  });
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: width * .45,
+      width: double.infinity,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: backgrounColor(),
+              child: Card(
+                color: ktransparent,
+                child: Column(
+                  children: <Widget>[
+                    kheight20,
+                    Text(
+                      DateFormat.jm().format(
+                        DateTime.now(),
+                      ),
+                      style: GoogleFonts.poppins(
+                        color: kwhite,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Image.asset('asset/image/icons8-rainbow-48.png'),
+                    kheight,
+                    Text(
+                      '31 °C',
+                      style: GoogleFonts.poppins(
+                        color: kwhite,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class WeatheDetailcardWidget extends StatelessWidget {
+  const WeatheDetailcardWidget({
+    Key? key,
+    required this.width,
+  }) : super(key: key);
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: width * .4,
+      width: double.infinity,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: backgrounColor(),
+              child: const Card(
+                color: ktransparent,
+                child: WeatherDetailsWidget(
+                  content: 'khdfjl',
+                  value: 'dfsksjl',
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
